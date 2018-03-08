3e9645495d45081238d4adfd0732341280a784cf
Abdoulrahime Diallo <diallo.abdoulrahime@gmail.com>
2018-03-08 02:14:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 var katzDeli = [];
 
+// function takeANumber(katzDeli, newPerson) {
+//   var position = katzDeli.length + 1;
+//   katzDeli.push(newPerson);
+//   return "Welcome, " + newPerson +". You are number "+ position +" in line.";
+// }
+
+//when a new person comes into katzDeli they get assigned a number instead of a position (i.e., the first person who comes in is 501, the next 502, then 503 and so forth). As more people come in, that number increases until the last person of the day comes in. Unlike a position, which can decrease as people leave, the assigned number is universal for the day and only continues to increase as more people come in. 
+
 function takeANumber(katzDeli, newPerson) {
-  var position = katzDeli.length + 1;
-  katzDeli.push(newPerson);
-  return "Welcome, " + newPerson +". You are number "+ position +" in line.";
+  
+  var counter = 0;
+  while (counter => 0) {
+   counter++;
+}
+  
+  
+  return "Welcome, " + newPerson +". You are number "+ counter + ".";
 }
 
 
